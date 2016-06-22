008c0de6f583b72f13d1907b0a25f8aa3f0ad501
zion959 <ziontran@gmail.com>
2016-06-22 06:49:30 -0500

    Add poly optimization (1/3)
    Vendor (2/3)
    Device (3/3)
    
    Original Author: ZION959
    
    Conflicts:
    	core/dumpvar.mk

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -81,11 +81,21 @@ $(info   KERNEL_TOOLCHAIN_USED=$(TARGET_GCC_VERSION_ARM))
 else
 $(info   KERNEL_TOOLCHAIN_USED=$(TARGET_GCC_VERSION))
 endif
-$(info   HOST_ARCH=$(HOST_ARCH))
-$(info   HOST_OS=$(HOST_OS))
-$(info   HOST_OS_EXTRA=$(HOST_OS_EXTRA))
-$(info   HOST_BUILD_TYPE=$(HOST_BUILD_TYPE))
-$(info   BUILD_ID=$(BUILD_ID))
-$(info   OUT_DIR=$(OUT_DIR))
-$(info ============================================)
+ifdef    CORTEX_TUNINGS
+$(info ${CLR_MAG}   CORTEX_TUNINGS=$(CORTEX_TUNINGS))
+else
+$(info ${CLR_MAG}   CORTEX_TUNINGS=true)
+endif
+ifdef    POLLY_OPTIMIZATION
+$(info ${CLR_MAG}   POLLY_OPTIMIZATION=$(POLLY_OPTIMIZATION))
+else
+$(info   POLLY_OPTIMIZATION=true)
+endif
+$(info ${CLR_YLW}   HOST_ARCH=$(HOST_ARCH))
+$(info ${CLR_YLW}   HOST_OS_EXTRA=$(HOST_OS_EXTRA))
+$(info ${CLR_YLW}   HOST_OS_EXTRA=$(HOST_OS_EXTRA))
+$(info ${CLR_YLW}   HOST_BUILD_TYPE=$(HOST_BUILD_TYPE))
+$(info ${CLR_YLW}   BUILD_ID=$(BUILD_ID))
+$(info ${CLR_YLW}   OUT_DIR=$(OUT_DIR))
+$(info ${CLR_CYN} ============================================)
 endif
